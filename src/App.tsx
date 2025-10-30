6077761078165068d17d400fee6d5b13ba9495a9
Kanepiro <hiro-k@hh.iij4u.or.jp>
2025-10-30 18:30:12 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import type { Group, MenuItem } from "./menuOptions";
 import { MENU_ITEMS_DEFAULT, byGroup, groupsOf } from "./menuOptions";
 
 // ---- Versioning ----
-const FIXED_VERSION_TEXT = "v2.1.060";
+const FIXED_VERSION_TEXT = "v2.1.061";
 const VERSION_PREFIX = "2.1"; // major.minor
 const STORAGE_VERSION_PATCH = "menu.version.patch";
 function loadVersionPatch(): number {
